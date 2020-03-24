b68f283c0cf692303ef4e5c1336838faf3c3d6f0
MathBoulot <mathieu.le.bellec@gmail.com>
2020-03-24 08:42:19 +0100

    style: Burger de la mort

--- src/pages/p-burger-de-la-mort/p-burger-de-la-mort-2.vue
+++ src/pages/p-burger-de-la-mort/p-burger-de-la-mort-2.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="b2lm ketchup">
-    <div class="panel-l">
-      <img src="./../../assets/burgerdelamort2.jpg" class="item photo" />
-    </div>
-    <div class="panel-r">
+    <div class="top">
+      <div class="title">
+        <h2>Le Burger de la</h2>
+        <h1>Mort</h1>
+      </div>
       <div class="buttons">
         <button type="button" name="button" @click="showReponse">Reponse</button>
         <button type="button" name="button" @click="nextQuestion">Next</button>
       </div>
-      <div class="questions" v-show="showGoodAnswers">
-        <div class="question" v-for="(question, index) in questions" v-bind:key="question">
-          <h2>{{index+1}}</h2>
-          <div class="g-q-r">
-            <p class="q">{{question.question}}</p>
-            <p class="r">Réponse: {{question.goodrep}}</p>
+    </div>
+
+    <div class="container-flex">
+      <div class="panel-l">
+        <div class="questions" v-show="showGoodAnswers">
+          <div class="question" v-for="(question, index) in questions" v-bind:key="question">
+            <div class="divif" v-if="index<5">
+              <h2>{{index+1}}</h2>
+              <div class="g-q-r">
+                <p class="q">{{question.question}}</p>
+                <p class="r">Réponse: {{question.goodrep}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="panel-r">
+        <div class="questions" v-show="showGoodAnswers">
+          <div class="question" v-for="(question, index) in questions" v-bind:key="question">
+            <div class="divif" v-if="index>4">
+              <h2>{{index+1}}</h2>
+              <div class="g-q-r">
+                <p class="q">{{question.question}}</p>
+                <p class="r">Réponse: {{question.goodrep}}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
