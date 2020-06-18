76ede6db554237a294e1b70dbaa0446da59935fa
MathPerso <mathieu.le.bellec@gmail.com>
2020-06-18 22:01:28 +0200

    Fix: deploy

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <div class="header">
       <img src="./assets/Burger-Quiz-logo.png" alt />
       <p>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@ import Router from './router.js';
 import Store from './store/index.js';
 import App from './App.vue';
 
+Vue.config.productionTip = false
+
 new Vue({
   store: Store,
   router: Router,
   render: h => h(App)
 }).$mount('#app');
+
+
+// new Vue({
+//   el: '#app',
+//   store: Store,
+//   router: Router,
+//   render: h => h(App),
+//   components: { App }
+// })
